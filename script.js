690a338012c9bd0b0e5832216b3d7cb89879da92
HuniaNadeem-DevIntern <hunia.intern@devsinc.com>
2023-07-12 17:39:57 +0500

    Fix:
    * resposiveness fixed
    
    * footer text color fixed

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 const headerInput = document.getElementById("header-input");
 const headerColor = document.getElementById("header-color")
+const headerTextColor = document.getElementById("header-text-color")
 const headerTextDiv = document.querySelector(".card-header")
 const headerText = document.getElementById("header")
 
 const footerInput = document.getElementById("footer-input");
 const footerColor = document.getElementById("footer-color")
+const footerTextColor = document.getElementById("footer-text-color")
 const footerTextDiv = document.querySelector(".card-footer")
 const footerText = document.getElementById("footer")
 
+const resetButton = document.querySelector('.fa-arrows-rotate')
 const chooseButton = document.getElementById("choose-file");
 const downloadButton = document.getElementById("download-file");
 const fileInputField = document.getElementById("file-input");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-
+const lineWidth = document.getElementById("line-width")
+const lineColor = document.getElementById("line-color")
 var isDrawing = false;
 
 
-window.addEventListener("load", ()=>{
+window.addEventListener("load", () => {
   updateHeader()
   canvas.width = canvas.offsetWidth 
   canvas.height = canvas.offsetHeight
+  setLineWidth()
 })
   
-
-
 const startDraw = (e) => {
   isDrawing = true
   context.beginPath()
@@ -42,30 +45,13 @@ const stopDraw = (e) => {
   isDrawing = false
 }
 
-
-canvas.addEventListener("mousedown", startDraw)
-canvas.addEventListener("mousemove", draw)
-canvas.addEventListener("mouseup", stopDraw)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const setLineWidth = () => {
+  context.lineWidth = lineWidth.value
+}
+const setLineColor = () => {
+  console.log(lineColor.value)
+  context.strokeStyle = lineColor.value
+}
 
 const updateHeader = () => {
   const textNode = document.createTextNode(headerInput.value);
@@ -76,6 +62,9 @@ const updateHeader = () => {
 }
 const updateHeaderColor = (e) => {
   headerTextDiv.style.backgroundColor = e.target.value;
+}
+const updateHeaderTextColor = (e) => {
+  headerText.style.color = e.target.value;
 }
 
 const updateFooter = () => {
@@ -88,7 +77,14 @@ const updateFooter = () => {
 const updateFooterColor = (e) => {
   footerTextDiv.style.backgroundColor = e.target.value;
 }
+const updateFooterTextColor = (e) => {
+  footerText.style.color = e.target.value;
+  document.getElementById("default-text").style.color = e.target.value
+}
 
+const resetCanvas = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 const openExplorer = (e) => {
   fileInputField.click();
@@ -128,8 +124,18 @@ const downloadCard = (e) => {
 
 headerInput.addEventListener("input", updateHeader);
 headerColor.addEventListener("input", updateHeaderColor);
+headerTextColor.addEventListener("input", updateHeaderTextColor);
 footerInput.addEventListener("input", updateFooter);
 footerColor.addEventListener("input", updateFooterColor);
+footerTextColor.addEventListener("input", updateFooterTextColor);
 chooseButton.addEventListener("click", openExplorer);
 fileInputField.addEventListener("change", setCanvasBackground);
 downloadButton.addEventListener("click", downloadCard);
+resetButton.addEventListener("click", resetCanvas)
+
+canvas.addEventListener("mousedown", startDraw)
+canvas.addEventListener("mousemove", draw)
+canvas.addEventListener("mouseup", stopDraw)
+lineWidth.addEventListener("input", setLineWidth)
+lineColor.addEventListener("input", setLineColor)
+
